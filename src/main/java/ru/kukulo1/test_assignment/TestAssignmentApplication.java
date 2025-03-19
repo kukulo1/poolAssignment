dddd98fd04d6f1af9db712588618cca3e46482a9
kukulo1 <kukulo1@Mail.ru>
2025-03-19 09:30:32 +0300

    init commit

---
+++ src/main/java/ru/kukulo1/test_assignment/TestAssignmentApplication.java
@@ -0,0 +1,13 @@
+package ru.kukulo1.test_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kukulo1/test_assignment/TestAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kukulo1.test_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
